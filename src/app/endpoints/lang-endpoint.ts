809968b9a01f98e17752a7560bdf20d9dbadaa2b
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-07-17 21:19:27 +0500

    poor caching

--- src/app/endpoints/lang-endpoint.ts
+++ src/app/endpoints/lang-endpoint.ts
@@ -3,9 +3,10 @@ import { graphqlRequest } from "../../utils/utils";
 import * as dotenv from "dotenv";
 import { LangInfo, fetchLangs } from "../../fetch";
 import { renderPieChart } from "../../render";
-import { CONFIG } from "../../utils";
 
 dotenv.config();
+
+let githubLangCache = "try again";
 
 let langCache = {
     data: Array<LangInfo>,
@@ -74,13 +75,12 @@ export const getGithubRepos = () => {
 };
 
 export const langEndpoint = async (req: Request, res: Response) => {
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.send(githubLangCache);
+
     const repos = await getGithubRepos();
     const langs = fetchLangs(repos.data);
-
-    const cacheSeconds = parseInt(process.env.CACHE_SECONDS as string);
-
-    res.setHeader("Content-Type", "image/svg+xml");
     const totalBytesCount = langs.reduce((acc, lang) => acc + lang.size, 0);
-
-    return res.send(renderPieChart(`GitHub: top languages (${totalBytesCount} b.)`, langs));
+    const content = renderPieChart(`GitHub: top languages (${totalBytesCount} b.)`, langs);
+    githubLangCache = content;
 };
--- src/app/endpoints/wakatime-endpoint.ts
+++ src/app/endpoints/wakatime-endpoint.ts
@@ -3,18 +3,19 @@ import { CONFIG, httpGetRequest } from "../../utils";
 import { fetchWakatime } from "../../fetch";
 import { renderPieChart } from "../../render";
 
+let wakatimeLangCache = "try again";
+
 const getWakatimeData = async () => {
     return httpGetRequest("https://wakatime.com/api/v1/users/Quasarity/stats", {});
 };
 
 export const wakatimeEndpoint = async (req: Request, res: Response) => {
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.send(wakatimeLangCache);
+
     const stats = await getWakatimeData();
     const langs = fetchWakatime(stats.data);
-
-    const cacheSeconds = parseInt(process.env.CACHE_SECONDS as string);
-
-    res.setHeader("Content-Type", "image/svg+xml");
     const totalTime = stats.data.data.categories[0].digital.slice(0, 3);
-
-    return res.send(renderPieChart(`Wakatime: top languages (${totalTime} h)`, langs, true));
+    let content = renderPieChart(`Wakatime: top languages (${totalTime} h.)`, langs, true);
+    wakatimeLangCache = content;
 };
